699d301d8825482dd2f526ab9afb828105f0d191
David Frederick <dfrederick79@gmail.com>
2020-10-28 10:16:45 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1463bd4eed584e8bf13425f46b6595b4.js"
+  "/portfolio/precache-manifest.a261895272e1543b4b2981e72f8cdd73.js"
 );
 
 self.addEventListener('message', (event) => {
