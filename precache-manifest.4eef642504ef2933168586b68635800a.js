a48177f7fd0e185aae091684fd8d44fa1e5aa643
David Frederick <dfrederick79@gmail.com>
2020-12-21 17:37:12 -0600

    Updates

---
+++ precache-manifest.4eef642504ef2933168586b68635800a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b05ea449a87cec4990bbb8d200e175",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ad3c0eab7ad9882e2fd0",
+    "url": "/portfolio/static/css/main.c7df3b06.chunk.css"
+  },
+  {
+    "revision": "fe8f42314924858c9b79",
+    "url": "/portfolio/static/js/2.10e52cc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.10e52cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3c0eab7ad9882e2fd0",
+    "url": "/portfolio/static/js/main.608b16d1.chunk.js"
+  },
+  {
+    "revision": "8edfc14ef3115da44c2f",
+    "url": "/portfolio/static/js/runtime-main.99525b3a.js"
+  },
+  {
+    "revision": "35426327bd537b7e8998c2f0075f149c",
+    "url": "/portfolio/static/media/BugTrackerBig.35426327.png"
+  },
+  {
+    "revision": "6b42c00e67cdfd41a5d7ea2dd832c8dd",
+    "url": "/portfolio/static/media/BurgerBuilderBig.6b42c00e.png"
+  },
+  {
+    "revision": "75b4167f9cd2ccf1df6ab245fe500590",
+    "url": "/portfolio/static/media/CSSPressMeBig.75b4167f.png"
+  },
+  {
+    "revision": "a0bc14af7966b73705077e3e5e9d30f2",
+    "url": "/portfolio/static/media/LamarUniversity.a0bc14af.jpg"
+  },
+  {
+    "revision": "eebfbdc7f47b99ab97c0dfd7bb4c3b4b",
+    "url": "/portfolio/static/media/OmnifoodBig.eebfbdc7.png"
+  },
+  {
+    "revision": "74e567f9b77f88e550764bddc0765251",
+    "url": "/portfolio/static/media/ResumeDavidFrederick.74e567f9.pdf"
+  },
+  {
+    "revision": "14a68d93f09715599ac24b3809999913",
+    "url": "/portfolio/static/media/TXARNG.14a68d93.svg"
+  },
+  {
+    "revision": "7fc7252c285aecebd839640e7ad35a10",
+    "url": "/portfolio/static/media/WeatherAppBig.7fc7252c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4830320cf37466e5afec3d57e4fd0885.js"
+  "/portfolio/precache-manifest.4eef642504ef2933168586b68635800a.js"
 );
 
 self.addEventListener('message', (event) => {
